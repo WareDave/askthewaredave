f2009e34a32d6d6a1513f656f796cc3742a91347
dave clark <davetheclark87@gmail.com>
2020-03-05 18:26:30 -0700

    dave

---
+++ ask_the_waredave.py
@@ -0,0 +1,89 @@
+import sys
+import random
+
+ans = True
+
+while ans:
+    question = input("Ask the WareDave a question: (press enter to quit) ")
+    
+    answers = random.randint(1,25)
+    
+    if question == "":
+        sys.exit()
+    
+    elif answers == 1:
+        print ("fuck you guys, I'm going home")
+    
+    elif answers == 2:
+        print ("we need to get the hell out of dodge")
+    
+    elif answers == 3:
+        print ("if it's going to be that kind of party")
+    
+    elif answers == 4:
+        print ("fuck off")
+    
+    elif answers == 5:
+        print ("your mother had a peg leg with a kick stand")
+    
+    elif answers == 6:
+        print ("han shot first")
+    
+    elif answers == 7:
+        print ("doom,doom,doom. Time to sing the doom song.")
+    
+    elif answers == 8:
+        print ("Good, Bad, im the guy with the gun.")
+		
+	elif answers == 9:
+		print ("time to start hooking")
+		
+	elif answers == 10:
+		print ("Batman")
+		
+	elif answers == 11:
+		print ("cum bubble, as in you are a.")
+		
+	elif answers == 12:
+		print ("balls")
+		
+	elif answers == 13:
+		print ("your vagina, it hangs like sleaves of wizard")
+		
+	elif answers == 14:
+		print ("you are sick. why would you ask that?")
+		
+	elif answers == 15:
+		print ("your grams faped to hittler")
+		
+	elif answers == 16:
+		print ("i donno, maybe get a cat.")
+		
+	elif answers == 17:	
+		print ("cup the balls")
+	
+	elif answers == 18:
+	    print ("you will die alone")
+		
+	elif answers == 19:
+		print ("you made it move, then you lost it. put the bag back on")
+		
+	elif answers == 20:
+		print ("try getting him drunk, he may not care that your ugly.")
+		
+	elif answers == 21:
+		print ("kill yourself")
+		
+	elif answers == 22:
+		print ("you mother was a hampster")
+		
+	elif answers == 23:
+		print ("no, you smell like jew candy")
+		
+	elif answers == 24:
+		print ("try the truck stop, you may have better luck.")
+		
+	elif answers == 25: 
+		print ("not metal.")
+		
+	 
